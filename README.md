e0becbacc03dc654d7c5d95d6c33bbc316251706
Daniel Thwaites <danthwaites30@btinternet.com>
2021-11-15 16:50:17 +0000

    Initialise project :tada:

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  <img
+    src="images/axyn.png"
+    alt="Axyn logo"
+  />
+</p>
+
+# Axyn for Matrix
---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/7e567a3d092b7de69cdf5deaeb8d9526de230916";
+    utils.url = "github:numtide/flake-utils";
+    flipgenic = {
+      url = "github:danth/flipgenic";
+      flake = false;
+    };
+  };
+  outputs = inputs@{ nixpkgs, utils, ... }:
+    utils.lib.eachSystem ["x86_64-linux"]
+    (system:
+      with nixpkgs.legacyPackages.${system}.python3Packages;
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        ngt = buildPythonPackage rec {
+          pname = "ngt";
+          version = "v1.12.3";
+          src = pkgs.fetchFromGitHub {
+            owner = "yahoojapan";
+            repo = "NGT";
+            rev = version;
+            sha256 = "d2DUnuSlnMhd/QDJZRJLXQvcat37dEM+s9Ci4KXxxvQ=";
+          };
+          postPatch = ''
+            substituteInPlace python/src/ngtpy.cpp \
+              --replace "NGT_VERSION" '"${version}"'
+          '';
+          preConfigure = ''
+            export HOME=$PWD
+            export LD_LIBRARY_PATH=${pkgs.ngt}/lib
+            cd python
+          '';
+          buildInputs = [ pkgs.ngt ];
+          propagatedBuildInputs = [ numpy pybind11 ];
+        };
+
+        en-core-web-md = buildPythonPackage rec {
+          pname = "en-core-web-md";
+          version = "3.0.0";
+          src = pkgs.fetchzip {
+            url =
+              "https://github.com/explosion/spacy-models/releases/download/en_core_web_md-3.0.0/en_core_web_md-3.0.0.tar.gz";
+            sha256 = "4UrUhHNVLHxbOdm3BIIetv4Pk86GzFoKoSnlvLFqesI=";
+          };
+          propagatedBuildInputs = [ spacy ];
+        };
+
+        flipgenic = buildPythonPackage rec {
+          name = "flipgenic";
+          src = inputs.flipgenic;
+          propagatedBuildInputs = [ ngt spacy sqlalchemy ];
+        };
+
+        axyn-matrix = buildPythonApplication rec {
+          name = "axyn-matrix";
+          src = ./.;
+          propagatedBuildInputs = [
+            en-core-web-md
+            flipgenic
+            matrix-nio
+          ];
+        };
+
+        axyn-matrix-app = utils.lib.mkApp {
+          drv = axyn-matrix;
+          exePath = "/bin/axyn_matrix";
+        };
+
+      in {
+        packages = { inherit axyn-matrix; };
+        defaultPackage = axyn-matrix;
+
+        apps.axyn-matrix = axyn-matrix-app;
+        defaultApp = axyn-matrix-app;
+      });
+}
