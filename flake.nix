169e1fceb87ba111992c09776e934edff4c32fd6
Daniel Thwaites <danthwaites30@btinternet.com>
2022-07-02 16:02:23 +0100

    Initialise Rust project :construction:

--- flake.nix
+++ flake.nix
@@ -1,11 +1,43 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = inputs@{ nixpkgs, utils, ... }:
-    utils.lib.eachSystem ["x86_64-linux"]
-    (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { });
+
+  outputs =
+    { self, nixpkgs, crane, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        craneLib = crane.lib.${system};
+
+        nonRustDependencies = {
+          buildInputs = with pkgs; [ ];
+          nativeBuildInputs = with pkgs; [ ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly ({
+          src = ./.;
+        } // nonRustDependencies);
+
+        package = craneLib.buildPackage ({
+          src = ./.;
+          inherit cargoArtifacts;
+        } // nonRustDependencies);
+
+      in {
+        packages.default = package;
+        apps.default = utils.lib.mkApp { drv = package; };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ cargo rustc ];
+        };
+      }
+    );
 }
