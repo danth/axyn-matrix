b9032b55041936e6a590e3ed6d8bfb0ac5c36546
Daniel Thwaites <danthwaites30@btinternet.com>
2022-07-02 15:57:35 +0100

    Remove Python code :boom:
    Ready to be written afresh in Rust.

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 <p align="center">
-  <img
-    src="axyn_matrix/images/axyn.png"
-    alt="Axyn logo"
-  />
+  <img src="images/axyn.svg" alt="Axyn logo" />
 </p>
 
 # Axyn for Matrix
@@ -10,42 +7,6 @@
 Axyn is a chatbot which learns from old messages - in addition to a bank of
 quotes from movies and other sources - and tries to reply to what you send it.
 
-![An example conversation showing Axyn's behaviour.](./example.jpg)
+![An example conversation showing Axyn's behaviour.](images/example.jpg)
 
 Axyn never writes anything original.
-
-## Data sources
-
-These are used to build Axyn's initial bank of responses.
-
-- [Cornell Movie Dialogs Corpus](https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html)
-- [Craigslist Bargains](https://worksheets.codalab.org/worksheets/0x453913e76b65495d8b9730d41c7e0a0c/)
-- The entire works of Shakespeare
-  [from BRIDGES Data](https://bridgesdata.herokuapp.com/api/datasets/shakespeare)
-
-## Environment Variables
-
-### `AXYN_MATRIX_STORE_PATH`
-
-The directory that should be used for the database and Matrix state storage.
-
-### `AXYN_MATRIX_HOMESERVER`
-
-The URL of the homeserver which we want to connect to.
-
-### `AXYN_MATRIX_USER_ID`
-
-The user which will be used when we log in to the homeserver.
-
-### `AXYN_MATRIX_USER_PASSWORD`
-
-Password for the user given in `AXYN_MATRIX_USER_ID`.
-
-### `AXYN_MATRIX_DEVICE_ID`
-
-A unique identifier that distinguishes this client instance.
-
-### `AXYN_MATRIX_DEVICE_NAME`
-
-The name to give to a new device. If the device ID already exists, its name
-will not be changed.
--- flake.nix
+++ flake.nix
@@ -2,104 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     utils.url = "github:numtide/flake-utils";
-    flipgenic = {
-      url = "github:danth/flipgenic";
-      flake = false;
-    };
   };
   outputs = inputs@{ nixpkgs, utils, ... }:
     utils.lib.eachSystem ["x86_64-linux"]
     (system:
-      with nixpkgs.legacyPackages.${system}.python3Packages;
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        ngtVersion = "1.14.4";
-        ngtSrc = pkgs.fetchFromGitHub {
-          owner = "yahoojapan";
-          repo = "NGT";
-          rev = "v${ngtVersion}";
-          sha256 = "Wa15AdLLzOL7qqCR2Bt+0i0YIgd6qKBmtEU9Q9/uiQI=";
-        };
-
-        ngt = pkgs.stdenv.mkDerivation rec {
-          pname = "NGT";
-          version = ngtVersion;
-          src = ngtSrc;
-
-          nativeBuildInputs = with pkgs; [ cmake ];
-          buildInputs = with pkgs; [ llvmPackages.openmp ];
-
-          __AVX2__ = 0;
-        };
-
-        ngtpy = buildPythonPackage rec {
-          pname = "NGTpy";
-          version = ngtVersion;
-          src = ngtSrc;
-
-          postPatch = ''
-            substituteInPlace python/src/ngtpy.cpp \
-              --replace "NGT_VERSION" '"${version}"'
-          '';
-
-          preConfigure = ''
-            export HOME=$PWD
-            export LD_LIBRARY_PATH=${ngt}/lib
-            cd python
-          '';
-
-          buildInputs = [ ngt ];
-          propagatedBuildInputs = [ numpy pybind11 ];
-        };
-
-        en-core-web-md = buildPythonPackage rec {
-          pname = "en-core-web-md";
-          version = "3.3.0";
-          src = pkgs.fetchzip {
-            url =
-              "https://github.com/explosion/spacy-models/releases/download/en_core_web_md-3.3.0/en_core_web_md-3.3.0.tar.gz";
-            sha256 = "sy+vff9STk6vBlWybdbsVL9+orANuUNJjma1ar+u30s=";
-          };
-          propagatedBuildInputs = [ spacy ];
-        };
-
-        flipgenic = buildPythonPackage rec {
-          name = "flipgenic";
-          src = inputs.flipgenic;
-          propagatedBuildInputs = [ ngtpy spacy sqlalchemy ];
-        };
-
-        initial-responder = pkgs.callPackage ./initial_responder {
-          inherit en-core-web-md flipgenic;
-        };
-
-        axyn-matrix = buildPythonApplication rec {
-          name = "axyn-matrix";
-          src = ./.;
-          propagatedBuildInputs = [
-            aiofiles
-            beautifulsoup4
-            en-core-web-md
-            flipgenic
-            matrix-nio
-          ];
-          postPatch = ''
-            substituteInPlace axyn_matrix/__main__.py \
-              --replace '@INITIAL_RESPONDER@' '${initial-responder}'
-          '';
-        };
-
-        axyn-matrix-app = utils.lib.mkApp {
-          drv = axyn-matrix;
-          exePath = "/bin/axyn_matrix";
-        };
-
-      in {
-        packages = { inherit axyn-matrix; };
-        defaultPackage = axyn-matrix;
-
-        apps.axyn-matrix = axyn-matrix-app;
-        defaultApp = axyn-matrix-app;
-      });
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { });
 }
